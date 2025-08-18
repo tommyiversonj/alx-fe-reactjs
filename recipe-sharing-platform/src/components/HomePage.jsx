635151acef7ecbd26f3db3edc906b3c8434af5c6
tommyiversonj <tommyiversonj@gmail.com>
2025-08-18 22:14:35 +0000

    Update

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import recipesData from "./data.json"; 
+import recipesData from "../data.json"; 
 
 
 const HomePage = () => {
--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
-import React, { useEffect } from 'react';
 import recipesData from './data.json';
 
 const RecipeDetail = () => {
